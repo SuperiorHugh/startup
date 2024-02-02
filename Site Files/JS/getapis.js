00715c82c9349a6f82becbb082befd335434f3cf
SuperiorHugh <hugh.takara@gmail.com>
2024-02-02 00:39:15 -0700

    added joke service

--- Site Files/JS/getapis.js
+++ Site Files/JS/getapis.js
@@ -8,3 +8,10 @@ fetch('https://random-word-api.herokuapp.com/word')
     let stringified = String(data);
     randomword.innerText = stringified.replace(stringified[0], stringified[0].toUpperCase());
 });
+
+fetch('https://v2.jokeapi.dev/joke/Any?type=twopart')
+.then(response => response.json())
+.then(data => {
+    jokesetup.innerText = "\"" + data.setup + "\"";
+    jokedelivery.innerText = data.delivery;
+})
