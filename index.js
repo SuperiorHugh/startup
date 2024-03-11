422be50a783ca26af6e4fd32c3ea370be704d429
SuperiorHugh <hugh.takara@gmail.com>
2024-03-10 23:33:09 -0600

    js serves index to uer

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static('.'));
 
 let apiRouter = express.Router();
 app.use('/api', apiRouter);
 
 apiRouter.get('/', (req, res) => {
-    res.send({data: "here's your data"});
+    res.json({data: "here's your data"});
 });
 
-
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: '.' });
+});
+console.log('test1')
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 })
