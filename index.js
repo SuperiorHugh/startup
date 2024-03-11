62be7625e7ad3de3baca379b6ba8ddff9dea6cac
SuperiorHugh <hugh.takara@gmail.com>
2024-03-10 22:44:25 -0600

    js app listening

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-var apiRouter = express.Router();
-app.use('/api')
+let apiRouter = express.Router();
+app.use('/api', apiRouter);
 
+apiRouter.get('/', (req, res) => {
+    res.send({data: "here's your data"});
+});
+
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+})
