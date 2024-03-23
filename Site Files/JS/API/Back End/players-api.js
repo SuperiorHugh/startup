d103a2b9694c369097c8bff0f037df83cc8ef776
SuperiorHugh <hugh.takara@gmail.com>
2024-03-22 21:54:27 -0600

    js updated code cleanliness

--- Site Files/JS/API/Back End/players-api.js
+++ Site Files/JS/API/Back End/players-api.js
@@ -5,13 +5,6 @@ const express = require('express');
 let router = express.Router();
 
 
-
-let players = [ // get from db //TODO
-    {username: 'medium baguete lord', emotesused: 32}, //placeholders for real players
-    {username: 'big baguette lord', emotesused: 56}, 
-    {username: 'aspiring baguette lord', emotesused: 3}
-];
-
 //args: email password
 router.post('/login', async (req, res) => {
     const player = await db.getPlayer(req.body.email);
